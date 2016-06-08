45e6096eef0e821f765a2a1291daf7771b554116
Mario Jose Nuñez Villacis <pepeseya@hotmail.com>
2016-06-08 17:38:57 -0500

    funcionando
    Ventana no ajustable

--- botones.py
+++ botones.py
@@ -3,6 +3,8 @@ class calculadora:
     def __init__(self,master):
         self.frame=Frame(master)
         self.frame.grid()
+        self.pantalla=Entry(master,width=35)
+        self.pantalla.grid(row=10,column=0)
         bts=["0","1","2","3","4","5","6","7","8","9","+","-","=","*","/","C"]
         r=1
         c=0
@@ -18,11 +20,19 @@ class calculadora:
     def calcular(self,valor):
         if valor=="=":
             todo="123456789+-*/"
+            if self.pantalla.get()[0] not in todo:
+                self.pantalla.insert(END,"Operacion novalida")
             try:
-                resultado=eval(self.dados.get())
+                resultado=eval(self.pantalla.get())
+                self.pantalla.insert(END,"="+str(resultado))
             except:
+                self.pantalla.insert(END,"Error")
         elif valor=="C":
+            self.pantalla.delete(0,END)
         else:
+            if "=" in self.pantalla.get():
+                self.pantalla.delete(0,END)
+            self.pantalla.insert(END,valor)
 
 root=Tk()
 root.title("CALCULADORA")
